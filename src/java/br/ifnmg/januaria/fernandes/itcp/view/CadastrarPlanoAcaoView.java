7dbb8b9c87a987adfbc1745f2adfa5c6b030e0cf
alisson <alisson@alisson-VirtualBox>
2016-07-31 20:51:26 -0300

    Exclusão e edição de meta e atividade dentro do cadastro de plano realizado

--- src/java/br/ifnmg/januaria/fernandes/itcp/view/CadastrarPlanoAcaoView.java
+++ src/java/br/ifnmg/januaria/fernandes/itcp/view/CadastrarPlanoAcaoView.java
@@ -20,7 +20,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import org.primefaces.component.tabview.TabView;
 import org.primefaces.context.RequestContext;
-import org.primefaces.event.SelectEvent;
 import org.primefaces.event.TabChangeEvent;
 
 /**
@@ -58,16 +57,24 @@ public class CadastrarPlanoAcaoView extends MensagensGenericas implements Serial
             //tabview.setActiveIndex(0);
         } catch (RuntimeException ex) {
             System.out.println("BEAN(ListarEmpreendimentosView): Erro ao Carregar lista de Planos: " + ex);
-            
+
         }
     }
 
     public void excluirMeta() {
         System.out.println("Número de atividades na lista: " + metaSelecionada.getAtividadePlanejadaList().size());
         metaBean.excluirBean(metaSelecionada);//Exclui a meta do BD
-        listaSalvarMetas = objSalvar.getMetaList();
         listaSalvarMetas.remove(metaSelecionada);//Exclui a meta da Tela
-        objSalvar.setMetaList(listaSalvarMetas);//Atualiza o plano da tela
+
+        System.out.println("A atividades na meta");
+        for (int i = 0; i < listaSalvarAtividades.size(); i++) {
+            System.out.println("RODOU: " + i);
+            if (listaSalvarAtividades.get(i).getMeta().equals(metaSelecionada)) {
+                System.out.println("Apagou a atividade: " + listaSalvarAtividades.get(i).getNome());
+                listaSalvarAtividades.remove(i);
+            }
+        }
+
         metaSelecionada = null;
         msgGrowDeleteGeneric();
     }
@@ -94,6 +101,10 @@ public class CadastrarPlanoAcaoView extends MensagensGenericas implements Serial
         } else if (fimAtividade.after(atividadeSalvar.getMeta().getPrazo())) {//SE FIM ATIVIDADE depois PRAZO META
             msgPanelErro("Erro na data", "A 'data de fim' da atividade deve ser antes ou igual ao prazo de sua meta!");
         } else {
+            System.out.println("ADD O OBJ: " + atividadeSalvar.getId());
+            if (listaSalvarAtividades.contains(atividadeSalvar)) {//ESTA EDITANDO
+                listaSalvarAtividades.remove(atividadeSalvar);
+            }
             atividadeSalvar = atividadeBean.salvarBean(atividadeSalvar);
             listaSalvarAtividades.add(atividadeSalvar);
             atividadeSalvar = new AtividadePlanejada();
@@ -104,24 +115,24 @@ public class CadastrarPlanoAcaoView extends MensagensGenericas implements Serial
     }
 
     public void salvarMeta() {
-        listaSalvarMetas = objSalvar.getMetaList();
-        if (listaSalvarMetas == null) {
-            listaSalvarMetas = new ArrayList();
-        }
-
         if (metaSalvar.getPrazo().before(objSalvar.getDataInicio())) {
             msgPanelErro("Erro na data", "O prazo da meta deve ser maior ou igual a 'data de início' do plano de ação!");
         } else if (metaSalvar.getPrazo().after(objSalvar.getDataFim())) {
             msgPanelErro("Erro na data", "O prazo da meta deve ser menor ou igual a 'data de fim' do plano de ação'!");
         } else {
-            metaSalvar.setPlanoAcao(objSalvar);
+            System.out.println("ADD O OBJ: " + metaSalvar.getIdMeta());
+            if (listaSalvarMetas.contains(metaSalvar)) {//ESTA EDITANDO
+                listaSalvarMetas.remove(metaSalvar);
+            } else {//ESTA CADASTRANDO
+                metaSalvar.setPlanoAcao(objSalvar);
+            }
+            metaSalvar = metaBean.salvarBean(metaSalvar);
             listaSalvarMetas.add(metaSalvar);
-            objSalvar.setMetaList(listaSalvarMetas);
             metaSalvar = new Meta();
-            System.err.println("Tamanho das metas: " + listaSalvarMetas.size());
+            System.out.println("Tamanho das metas: " + listaSalvarMetas.size());
             RequestContext context = RequestContext.getCurrentInstance();
             context.execute("PF('wVarEditarDialog').hide()");
-            salvarView();
+            msgGrowSaveGeneric();
         }
     }
 
@@ -149,15 +160,24 @@ public class CadastrarPlanoAcaoView extends MensagensGenericas implements Serial
             return "";
         }
     }
-    
-    public void onTabChange(TabChangeEvent event){//Esse método é necessário para que o "tabView" do bean esteja atualizado com o que esta na tela
+
+    public void onTabChange(TabChangeEvent event) {//Esse método é necessário para que o "tabView" do bean esteja atualizado com o que esta na tela
         System.out.println("Usuário esta na tab: " + event.getTab().getId());
-        if(event.getTab().getId().equals("tabPlano")){
+        if (event.getTab().getId().equals("tabPlano")) {
             tabview.setActiveIndex(0);
-        }else if(event.getTab().getId().equals("tabMetas")){
+        } else if (event.getTab().getId().equals("tabMetas")) {
             tabview.setActiveIndex(1);
-        }else{
+        } else {
             tabview.setActiveIndex(2);
+        }
+    }
+
+    public void transfereObj() {//Para botão de editar
+        System.out.println("Usuário esta na aba: " + tabview.getActiveIndex());
+        if (tabview.getActiveIndex() == 1) {//Se estiver na aba de metas
+            metaSalvar = metaSelecionada;
+        } else {//senão, esta na aba de atividades
+            atividadeSalvar = atividadeSelecionada;
         }
     }
 
@@ -232,5 +252,13 @@ public class CadastrarPlanoAcaoView extends MensagensGenericas implements Serial
 
     public void setTabview(TabView tabview) {
         this.tabview = tabview;
+    }
+
+    public List<Meta> getListaSalvarMetas() {
+        return listaSalvarMetas;
+    }
+
+    public void setListaSalvarMetas(List<Meta> listaSalvarMetas) {
+        this.listaSalvarMetas = listaSalvarMetas;
     }
 }
