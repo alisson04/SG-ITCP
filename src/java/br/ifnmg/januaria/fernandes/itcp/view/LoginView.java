8853813c02bbe41c7d9fff6ea69182752cb9bb8e
alisson <alisson@alisson-VirtualBox>
2016-09-07 11:18:38 -0300

    Colocando imagem de fundo da aplicação
    Concluindo o cadastro inicial de corredenador.

--- src/java/br/ifnmg/januaria/fernandes/itcp/view/LoginView.java
+++ src/java/br/ifnmg/januaria/fernandes/itcp/view/LoginView.java
@@ -7,10 +7,13 @@ import br.ifnmg.januaria.fernandes.itcp.util.MensagensGenericas;
 import br.ifnmg.januaria.fernandes.itcp.util.SessionUtil;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Random;
 import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.faces.context.FacesContext;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -20,18 +23,33 @@ import javax.faces.context.FacesContext;
 @Named("LoginView")
 public class LoginView extends MensagensGenericas implements Serializable {
 
-    private Usuario usuarioLogado = new Usuario();
-    private LoginBean bean = new LoginBean();
+    private Usuario usuarioLogado;
+    private Usuario obj;
+    private LoginBean bean;
     private UsuarioBean usrBean;
     private boolean existeUser;
 
     public LoginView() {
+        bean = new LoginBean();
+        obj = new Usuario();
+        usuarioLogado = new Usuario();
         usrBean = new UsuarioBean();
         existeUser = true;
 
         if (usrBean.contarLinhasBean() == 0) {
             existeUser = false;
         }
+    }
+
+    public void salvarCoordenador() {
+        obj.setStatusSistema("Ativo");//SETA O STATUS
+        obj.setSenha(gerarSenhaAleatoria());//GERA A SENHA ALEATORIA e SETA
+        obj.setCargo("Coordenador");//SETA O CARGO
+        usrBean.enviarEmail(obj.getEmail(), "Sistema Sigitec", "Sua senha é: " + obj.getSenha());//Manda o emaill
+        obj.setSenha(DigestUtils.md5Hex(obj.getSenha()));//CRIPTOGRAFA A SENHA ALEATORIA
+        usrBean.salvarBean(obj);//Salva o usuário
+        obj = new Usuario();//Limpa o usuário salvo
+        msgGrowSaveGeneric();
     }
 
     public String logar() {
@@ -58,7 +76,21 @@ public class LoginView extends MensagensGenericas implements Serializable {
     public String sair() {
         return bean.sair(usuarioLogado);
     }
+    
+    public String gerarSenhaAleatoria() {
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVYWXZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        String senhaAleatoria = "";
+        int index = -1;
+        for (int i = 0; i < 10; i++) {
+            index = random.nextInt(letras.length());
+            senhaAleatoria += letras.substring(index, index + 1);
+        }
+        System.out.println("SENHA: " + senhaAleatoria);
+        return senhaAleatoria;
+    }
 
+    //SETS E GETS
     public Usuario getUsuarioLogado() {
         return usuarioLogado;
     }
@@ -69,5 +101,13 @@ public class LoginView extends MensagensGenericas implements Serializable {
 
     public boolean isExisteUser() {
         return existeUser;
+    }
+
+    public Usuario getObj() {
+        return obj;
+    }
+
+    public void setObj(Usuario obj) {
+        this.obj = obj;
     }
 }
