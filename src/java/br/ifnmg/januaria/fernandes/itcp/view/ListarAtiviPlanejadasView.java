37faaeb164cd86685535bc58eee761ca0dfcec97
alisson <alisson@alisson-VirtualBox>
2016-11-06 19:55:01 -0200

    Modificador filtro da listagem de atividades

--- src/java/br/ifnmg/januaria/fernandes/itcp/view/ListarAtiviPlanejadasView.java
+++ src/java/br/ifnmg/januaria/fernandes/itcp/view/ListarAtiviPlanejadasView.java
@@ -1,11 +1,17 @@
 package br.ifnmg.januaria.fernandes.itcp.view;
 
 import br.ifnmg.januaria.fernandes.itcp.bean.AtividadePlanejadaBean;
+import br.ifnmg.januaria.fernandes.itcp.bean.EmpreendimentoBean;
 import br.ifnmg.januaria.fernandes.itcp.bean.MetaBean;
+import br.ifnmg.januaria.fernandes.itcp.bean.PlanoAcaoBean;
 import br.ifnmg.januaria.fernandes.itcp.domain.AtividadePlanejada;
+import br.ifnmg.januaria.fernandes.itcp.domain.Empreendimento;
 import br.ifnmg.januaria.fernandes.itcp.domain.Meta;
+import br.ifnmg.januaria.fernandes.itcp.domain.PlanoAcao;
+import br.ifnmg.januaria.fernandes.itcp.util.MensagensGenericas;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
@@ -15,7 +21,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
-import org.primefaces.event.SelectEvent;
 
 /**
  *
@@ -23,33 +28,80 @@ import org.primefaces.event.SelectEvent;
  */
 @ManagedBean(name = "ListarAtiviPlanejadasView")
 @ViewScoped
-public class ListarAtiviPlanejadasView implements Serializable {
+public class ListarAtiviPlanejadasView extends MensagensGenericas implements Serializable {
 
     private AtividadePlanejada objSelecionado;
     private AtividadePlanejada objSalvar = new AtividadePlanejada();
-    private List<Meta> listaSelectOneMenu;
+    private AtividadePlanejadaBean bean = new AtividadePlanejadaBean();
     private List<AtividadePlanejada> listaObjs;
     private List<AtividadePlanejada> listaObjsFiltradas;
-    private AtividadePlanejadaBean bean = new AtividadePlanejadaBean();
+
+    private List<PlanoAcao> listaPlanos;
+    private PlanoAcao planoSelecionado;
+    private PlanoAcaoBean planoBean;
+
+    private List<Meta> listaMetas;
+
     private MetaBean metaBean = new MetaBean();
+    private Meta metaSelecionada;//Recebe a seleção do SelectOneMenu
+
+    private List<Empreendimento> listaEmpreendimentos;
+    private EmpreendimentoBean eptBean;
+    private Empreendimento eesSelecionado;//Recebe a seleção do SelectOneMenu
 
     public ListarAtiviPlanejadasView() {
         try {
+            planoBean = new PlanoAcaoBean();
+            listaPlanos = planoBean.listarBean();
+
             listaObjs = bean.listarBean();
-            listaSelectOneMenu = metaBean.listarBean();
+            listaMetas = metaBean.listarBean();
+            metaSelecionada = new Meta();
+
+            eptBean = new EmpreendimentoBean();
+            listaEmpreendimentos = eptBean.listarBean();
         } catch (RuntimeException ex) {
             Logger.getLogger(ListarPlanoAcaoView.class.getName()).log(Level.SEVERE, null, ex);
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "Erro inesperado", "Erro ao tentar listar as atividades, contate o administrador do sistema!");
-            RequestContext.getCurrentInstance().showMessageInDialog(message);
+            msgPanelErroInesperadoGeneric();
         }
     }
-    
-    public void transfereObj(){//Para botão de editar
+
+    public void filtraPlanosPorEes() {//Acontece ao selecionar um EES
+        List<PlanoAcao> listaAux = new ArrayList();
+
+        if (eesSelecionado != null) {
+            for (int i = 0; i < listaPlanos.size(); i++) {
+                if (listaPlanos.get(i).getEmpreendimento().equals(eesSelecionado)) {
+                    listaAux.add(listaPlanos.get(i));
+                }
+            }
+            listaPlanos = listaAux;
+        } else {
+            listaPlanos = planoBean.listarBean();
+        }
+
+    }
+
+    public void filtraMetasPorPlano() {//Acontece ao selecionar um Plano
+        List<Meta> listaAux = new ArrayList();
+        
+        if (planoSelecionado != null) {
+            for (int i = 0; i < listaMetas.size(); i++) {
+                if (listaMetas.get(i).getPlanoAcao().equals(planoSelecionado)) {
+                    listaAux.add(listaMetas.get(i));
+                }
+            }
+            listaMetas = listaAux;
+        } else {
+            listaMetas = metaBean.listarBean();
+        }
+    }
+
+    public void transfereObj() {//Para botão de editar
         objSalvar = objSelecionado;
     }
 
-    public void reiniciaObj(){//Para botão de cadastrar
+    public void reiniciaObj() {//Para botão de cadastrar
         System.out.println("objSalvar Reiniciado ====================== ");
         objSalvar = new AtividadePlanejada();
     }
@@ -91,9 +143,8 @@ public class ListarAtiviPlanejadasView implements Serializable {
             return "";
         }
     }
-    
-    //SETS E GETS
 
+    //SETS E GETS
     public AtividadePlanejada getObjSelecionado() {
         return objSelecionado;
     }
@@ -102,12 +153,12 @@ public class ListarAtiviPlanejadasView implements Serializable {
         this.objSelecionado = objSelecionado;
     }
 
-    public List<Meta> getListaSelectOneMenu() {
-        return listaSelectOneMenu;
+    public List<Meta> getListaMetas() {
+        return listaMetas;
     }
 
-    public void setListaSelectOneMenu(List<Meta> listaSelectOneMenu) {
-        this.listaSelectOneMenu = listaSelectOneMenu;
+    public void setListaMetas(List<Meta> listaMetas) {
+        this.listaMetas = listaMetas;
     }
 
     public List<AtividadePlanejada> getListaObjs() {
@@ -132,5 +183,45 @@ public class ListarAtiviPlanejadasView implements Serializable {
 
     public void setObjSalvar(AtividadePlanejada objSalvar) {
         this.objSalvar = objSalvar;
+    }
+
+    public List<Empreendimento> getListaEmpreendimentos() {
+        return listaEmpreendimentos;
+    }
+
+    public void setListaEmpreendimentos(List<Empreendimento> listaEmpreendimentos) {
+        this.listaEmpreendimentos = listaEmpreendimentos;
+    }
+
+    public Meta getMetaSelecionada() {
+        return metaSelecionada;
+    }
+
+    public void setMetaSelecionada(Meta metaSelecionada) {
+        this.metaSelecionada = metaSelecionada;
+    }
+
+    public Empreendimento getEesSelecionado() {
+        return eesSelecionado;
+    }
+
+    public void setEesSelecionado(Empreendimento eesSelecionado) {
+        this.eesSelecionado = eesSelecionado;
+    }
+
+    public List<PlanoAcao> getListaPlanos() {
+        return listaPlanos;
+    }
+
+    public void setListaPlanos(List<PlanoAcao> listaPlanos) {
+        this.listaPlanos = listaPlanos;
+    }
+
+    public PlanoAcao getPlanoSelecionado() {
+        return planoSelecionado;
+    }
+
+    public void setPlanoSelecionado(PlanoAcao planoSelecionado) {
+        this.planoSelecionado = planoSelecionado;
     }
 }
